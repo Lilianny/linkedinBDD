0dd4f4dbe83310c89f15ca1ebe7948c02d44a87d
lilianny <liliannyrodriguez@gmail.com>
2017-09-24 00:51:33 -0300

    Se agrega un espacio para una segunda prueba

--- src/test/java/bddTest/LoginRunTest.java
+++ src/test/java/bddTest/LoginRunTest.java
@@ -8,6 +8,6 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/login", snippets = SnippetType.CAMELCASE)
-public class LoginRunTest {
+public class LoginRunTest { 
 
 }
